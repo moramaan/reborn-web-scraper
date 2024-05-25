85b41ec108c5c9dc6e32dbd97fbc7b6ec1c0a65a
moramaan <dmorahidalgo@gmail.com>
2024-05-25 14:29:43 +0200

    Improve readability and reserved attribute. Handle timeoutError searching condition.

--- scrape.js
+++ scrape.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const fs = require("fs");
 const { chromium } = require("playwright");
@@ -13,14 +13,17 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
+// Function to scrape the main page for product listings
 async function scrapePage(url) {
   const browser = await chromium.launch();
   const page = await browser.newPage();
 
   try {
+    // Navigate to the URL
     await page.goto(url);
     await page.waitForSelector(".ItemCardList__item");
 
+    // Extract product details from the page
     const products = await page.evaluate(() => {
       const productListings = Array.from(
         document.querySelectorAll(".ItemCardList__item")
@@ -45,6 +48,7 @@ async function scrapePage(url) {
   }
 }
 
+// Function to upload images to Cloudinary and get secure URLs
 async function uploadImagesToCloudinary(imageUrls, productId) {
   const uploadPromises = imageUrls.map((imageUrl) => {
     return cloudinary.uploader.upload(imageUrl, {
@@ -56,10 +60,11 @@ async function uploadImagesToCloudinary(imageUrls, productId) {
   return uploadResults.map((result) => result.secure_url);
 }
 
-async function scrapeImagesAndDescriptionFromUrls(products) {
+// Function to scrape product details from individual product pages
+async function scrapProductDetails(products) {
   const browser = await chromium.launch();
   const page = await browser.newPage();
-  const productsWithImagesAndDescription = [];
+  const arrProductsWithDetails = [];
 
   try {
     for (const product of products) {
@@ -70,6 +75,7 @@ async function scrapeImagesAndDescriptionFromUrls(products) {
       await page.waitForSelector("wallapop-carousel");
       console.log("Wallapop carousel loaded");
 
+      // Extract image URLs from the page
       const imageUrls = await page.evaluate(() => {
         const imageElements = document.querySelectorAll(
           'wallapop-carousel img[slot="carousel-content"]'
@@ -78,6 +84,7 @@ async function scrapeImagesAndDescriptionFromUrls(products) {
       });
       console.log("Image URLs:", imageUrls);
 
+      // Extract product description from the page
       await page.waitForSelector(".item-detail_ItemDetail__description__7rXXT");
       const description = await page.evaluate(() => {
         const descriptionElement = document.querySelector(
@@ -87,32 +94,51 @@ async function scrapeImagesAndDescriptionFromUrls(products) {
       });
       console.log("Description:", description);
 
-      await page.waitForSelector(
-        ".item-detail-additional-specifications_ItemDetailAdditionalSpecifications__characteristics__Ut9iT"
-      );
-      const condition = await page.evaluate(() => {
-        const conditionElement = document.querySelector(
-          ".item-detail-additional-specifications_ItemDetailAdditionalSpecifications__characteristics__Ut9iT"
+      // Attempt to extract product condition, handle case where element may not be found
+      let condition = "";
+      try {
+        await page.waitForSelector(
+          ".item-detail-additional-specifications_ItemDetailAdditionalSpecifications__characteristics__Ut9iT",
+          { timeout: 5000 } // Reduced timeout to 5 seconds
         );
-        return conditionElement ? conditionElement.textContent.trim() : "";
-      });
-      console.log("State:", condition);
+        condition = await page.evaluate(() => {
+          const conditionElement = document.querySelector(
+            ".item-detail-additional-specifications_ItemDetailAdditionalSpecifications__characteristics__Ut9iT"
+          );
+          return conditionElement ? conditionElement.textContent.trim() : "";
+        });
+        console.log("Condition:", condition);
+      } catch (timeoutError) {
+        console.log("Condition element not found, skipping...");
+      }
 
-      // Upload images to Cloudinary
+      // Check for "Reservado" badge to determine if the product is reserved
+      const isReserved = await page.evaluate(() => {
+        const badgeElement = document.querySelector(
+          'wallapop-badge[badge-type="reserved"][text="Reservado"]'
+        );
+        return badgeElement !== null;
+      });
+      console.log("Is Reserved:", isReserved);
+
+      // Upload images to Cloudinary and get their secure URLs
       const cloudinaryUrls = await uploadImagesToCloudinary(
         imageUrls,
-        product.uuid
+        product.id
       );
 
-      const productWithImagesAndDescription = {
+      // Create a product object with all extracted details and add it to the list
+      const productWithDetails = {
         ...product,
         description,
         condition,
-        images: cloudinaryUrls, // Use Cloudinary URLs
+        reserved: isReserved,
+        images: cloudinaryUrls, 
       };
-      productsWithImagesAndDescription.push(productWithImagesAndDescription);
+      arrProductsWithDetails.push(productWithDetails);
 
-      if (productsWithImagesAndDescription.length >= 10) {
+      // Stop after processing 10 products
+      if (arrProductsWithDetails.length >= 10) {
         break;
       }
     }
@@ -122,9 +148,10 @@ async function scrapeImagesAndDescriptionFromUrls(products) {
     await browser.close();
   }
 
-  return productsWithImagesAndDescription;
+  return arrProductsWithDetails;
 }
 
+// Main function to coordinate the scraping process
 async function main() {
   try {
     const url = process.argv[2];
@@ -137,18 +164,21 @@ async function main() {
     const first10Products = await scrapePage(url);
 
     if (first10Products.length > 0) {
-      const productsWithImagesAndDescription =
-        await scrapeImagesAndDescriptionFromUrls(first10Products);
+      const arrProductsWithDetails = await scrapProductDetails(
+        first10Products
+      );
 
+      // Create a results directory if it doesn't exist
       const resultsDir = path.join(__dirname, "results");
       if (!fs.existsSync(resultsDir)) {
         fs.mkdirSync(resultsDir);
       }
-
+      
+      // Save the results to a file
       const filePath = path.join(resultsDir, "reborn_scraped_products.json");
       fs.writeFileSync(
         filePath,
-        JSON.stringify(productsWithImagesAndDescription, null, 2)
+        JSON.stringify(arrProductsWithDetails, null, 2)
       );
       console.log(`Data saved to ${filePath}`);
     } else {
